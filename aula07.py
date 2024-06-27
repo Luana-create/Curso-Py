5c79a779dde265ce8398ad9fbcf18beb557b47b0
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-06-27 17:03:26 -0300

    Create aula07.py

---
+++ aula07.py
@@ -0,0 +1,20 @@
+#OPERAÇÕES:
+# adiçao +
+#subtração -
+#multiplicação * 
+#divisão /
+#exponenciação **
+#Divisão inteira //
+#Resto da divisao % 
+#5+2==7
+#5-2==3
+#5*2==10
+#5/2==2.5
+#5**2==25
+#5//2==2
+#5%2==1
+#Ordem de Precedência
+#1º ()
+#2° **
+#3º* / // %
+#4º + - 
