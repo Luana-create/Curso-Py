99d1c5a1b99bcc6a6d3a2e6d687ec2746806e731
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-06-27 16:08:46 -0300

    003e004
    Dividi a resolução de 1 exercício em 2
    o 003
    o 004

---
+++ 004.py
@@ -0,0 +1,8 @@
+a= input('Digite algo ')#a é um objeto
+print('O tipo primitivo desse valor é? ', type(a))
+print('Só tem espaços? ', a.isspace())#são métodos de a todos esses is
+print("É alfanumerico? ", a.isalnum())
+print('É um alfabetico? ', a.isalpha())
+print ('Esta em maiúscula? ', a.isupper())
+print ('Esta em minúscula? ', a.islower())
+print('Esta capitalizado? ', a.istitle())#Nem maiuscula e nem minúscula
