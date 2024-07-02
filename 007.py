12f9f7e8cb4c2dd72110b57e2b8900383a2fe640
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-07-02 16:37:27 -0300

    Atualizar o 007.py
    Fazendo ex direto no github sem editor.

--- 007.py
+++ 007.py
@@ -1,2 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
 # Cuidado com a ordem de precedencia.
+
+nota1=float(input("Digite a primeira nota: ")
+nota2=float(input("Digite a segunda nota:  ")
+media=(nota1+nota2)/2
+print("A média do aluno é:{:.2f}".format(media))
