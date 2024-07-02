4003359af83592d3aef3abe73224301ea6a5c902
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-07-02 16:51:07 -0300

    Atualizar o 008.py
    Ex feito direto no github sem editor.

--- 008.py
+++ 008.py
@@ -1,1 +1,5 @@
 #Escreva um programa que leia um valor em metros e exiba convertido em centímetro e milimetros.
+val-metros=float(input("Digite o valor que deseja converter: "))
+val-cent= val-metros/100
+val-mili= val-metros/1000
+print("Este valor em centímetros é {:.2f} e em milímetros é {:.2f}".format(val-cent, val-mili))
