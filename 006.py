55ce5530697b3cb85d30cda52c9fa68258a84adb
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-07-04 15:37:32 -0300

    Corrigido erros
    Erros na escrita do código, fechar parentes e os nomes das variáveis estavam em um formato não aceito.

--- 006.py
+++ 006.py
@@ -1,5 +1,5 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
-n=int(float(input("Difite um número:")))
+n=int(float(input("Digite um número:")))
 n1=n*2
 n2=n*3
 n3=n**(1/2)
