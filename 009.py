cc7ec5d15ebd5f9774945c15465e08809f2fa920
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-07-05 22:08:10 -0300

    Estilizando
    Tornei o código mais estiloso e espaçado.

--- 009.py
+++ 009.py
@@ -1,7 +1,7 @@
 # Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 n = int(input('Digite um número para obter sua tabuada:'))
-print('A tabuada de {} é:\n'.format(n))
-print('_')*12
+print('A tabuada de {} é:'.format(n))
+print('_'*12)
 print('{} x {:2}= {}'.format(n, 1, n*1))
 print('{} x {:2}= {}'.format(n, 2, n*2))
 print('{} x {:2}= {}'.format(n, 3, n*3))
@@ -13,4 +13,4 @@ print('{} x {:2}= {}'.format(n, 8, n*8))
 print('{} x {:2}= {}'.format(n, 9, n*9))
 print('{} x {:2}= {}'.format(n, 10, n*10))
 print('{} x {:2}= {}'.format(n, 11, n*11))
-print('_')*12
+print('_'*12)
--- 010.py
+++ 010.py
@@ -1,1 +1,4 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+din=float(input('Quanto dinheiro você tem na carteira?R$'))
+conv= din/3.27#Hoje tá 5,46 em julho de 2024
+print("Com {} pode comprar até {:2} dolares com este valor".format(din, conv))
