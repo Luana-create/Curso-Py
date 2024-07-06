82477502f05adea45c1f8628d5e3e8e82792907f
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-07-05 22:54:28 -0300

    Update 012.py
    Exercício parece simples, mas não é.

--- 012.py
+++ 012.py
@@ -1,1 +1,6 @@
 #Faça um algoritmo que mostra o preço de um produto e mostre seu novo preço, com desconto de 5% de desconto.
+x=float(input("Valor original do produto:"))
+d= (x*0.05)
+vd= x-d
+print("O produto que custava {}, com desconto de 5% para a custar {}".format(x, vd))
+
