648d4e040163192f859cbc20953e2c0a5b5c4ce0
Luana Ferreira <105221599+Luana-create@users.noreply.github.com>
2024-07-05 22:40:14 -0300

    Exercícios
    Resolução de exercícios e estilizada.
    Comparando minha lógica com a do professor.

--- 010.py
+++ 010.py
@@ -1,4 +1,7 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
-din=float(input('Quanto dinheiro você tem na carteira?R$'))
-conv= din/3.27#Hoje tá 5,46 em julho de 2024
-print("Com {} pode comprar até {:2} dolares com este valor".format(din, conv))
+real=float(input('Quanto dinheiro você tem na carteira?R$'))
+dolar= real/3.27#Hoje tá 5,46 em julho de 2024
+print("Com R${:.2f} você pode comprar até US${:.2f} dolares com este valor".format(real, dolar))
+#OBS sempre esqueço na hora de formatar o 2f de colocar o .
+#Se esquecer vai bugar completamente o codigo.
+#Tem que ser :.2f
--- 011.py
+++ 011.py
@@ -1,1 +1,7 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+larg=float(input('Qual a largura da parede em metros?' ))
+alt=float(input("Qual a altura da parede em metros?" ))
+area=larg*alt
+litros=area/2
+print('Sua parede tem as dimensões {:.2f} x {:.2f} e sua área é de {:.2f}m².\nSerá necessário {:.2f} litros de tinta para pintar os {:.2f}m²'.format(larg, alt,area, litros, area))
+#Poderia ter optado por fazer dois print ao invés de usar \n, mas achei mais prático.
